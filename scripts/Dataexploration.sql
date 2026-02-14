c2d0d2c70df76ea90263552ff38624d857f0abff
Shreyaas Dhamore <shreyassureshdhamore1@gmail.com>
2026-02-14 16:48:35 +0530

    Update Dataexploration.sql

--- scripts/Dataexploration.sql
+++ scripts/Dataexploration.sql
@@ -13,3 +13,12 @@ EXTRACT(YEAR from max(order_date)) -  EXTRACT(YEAR from min(order_date))  as ord
 (DATE_PART('year', MAX(order_date)) - DATE_PART('year', MIN(order_date))) * 12 
 + (DATE_PART('month', MAX(order_date)) - DATE_PART('month', MIN(order_date))) AS order_range_months
 from gold.fact_sales
+
+
+--find oldest and yongest age in our customer and their age 
+
+select 
+max(age(current_date,birthday)) as oldest_customer,
+min(age(current_date,birthday)) as youngest_customer
+from 
+gold.dim_customer
